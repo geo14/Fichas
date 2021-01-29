7d47c1198b7ca050b9fac7a85917e5ea1a5b2c1a
Geovanny Zuniga <geova1402@gmail.com>
2021-01-29 11:16:20 -0600

    Adding new changes in python script

--- generarFichaPython.py
+++ generarFichaPython.py
@@ -11,15 +11,15 @@ store = sys.argv[3]
 loginData =  {'txtEmail': email, 'txtClave': password}
 
 response = session.get('https://www.fichascr.com/GeneradorFichas.php?CodTienda=' + store)
-print(response.text)
+#print(response.text)
 
 while True:
     seconds = pd.datetime.now().second
     print("Esperando para ingresar... ", seconds)
     if seconds == 0:
         response = session.post('https://www.fichascr.com/VerificaIngreso.php', loginData)
-        print(len(response.content))
-        if len(response.content) < 4200:
+        #print(len(response.content))
+        if len(response.content) > 4200:
             response = session.get('https://www.fichascr.com/GeneraFichas.php')
             print(response.text)
         break
