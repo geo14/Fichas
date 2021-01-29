d2209559d31d9f3b1c5c5ba5a5838f1cff60b45d
Geovanny Zuniga <geova1402@gmail.com>
2021-01-29 11:30:39 -0600

    Adding users and passwords

--- generarFichaPython.py
+++ generarFichaPython.py
@@ -4,9 +4,41 @@ import sys
 
 session = requests.Session()
 
-email = sys.argv[1]
-password = sys.argv[2]
-store = sys.argv[3]
+user = sys.argv[1]
+store = sys.argv[2]
+password = ""
+email = ""
+
+if user == "Geova":
+    email = "geova1402@gmail.com"
+    password = "geova"
+elif user == "Kei":
+    email = "keisanchez2003@gmail.com"
+    password = "tesorokei"
+elif user == "Lavi":
+    email = "lavi0707@hotmail.com"
+    password = "colquin"
+elif user == "Colco":
+    email = "gzuniga62@hotmail.com"
+    password = "geolavi"
+elif user == "Dayi":
+    email = "coralcolette@hotmail.com"
+    password = "tesorodayi"
+elif user == "Vale":
+    email = "vale.roldan2505@gmail.com"
+    password = "tesorovale"
+elif user == "Nana":
+    email = "kemblyroldan07@gmail.com"
+    password = "tesoronana"
+elif user == "Zayi":
+    email = "zayira1382@gmail.com"
+    password = "tesorozayi"
+elif user == "Jonni":
+    email = "jonnigomezcastro1212@gmail.com"
+    password = "tesoro"
+elif user == "Ventas":
+    email = "ventasdiamante18@gmail.com"
+    password = "tesoro"
 
 loginData =  {'txtEmail': email, 'txtClave': password}
 
